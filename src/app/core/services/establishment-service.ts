1d127511b955221d1dacb5e11d4e30a54cfcb2d1
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-05-22 19:04:25 -0400

    Handling the prod vs dev environment variables

--- src/app/core/services/establishment-service.ts
+++ src/app/core/services/establishment-service.ts
@@ -2,25 +2,30 @@ import { Injectable } from "@angular/core";
 import { Establishment } from "../../pages/establishment/establishment";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { map, Observable } from "rxjs";
+import { environment } from "../../../environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class EstablishmentService {
-  private apiUrl = "http://localhost:8080/establishments";
-
   constructor(private http: HttpClient) {}
 
   getList(): Observable<Establishment[]> {
-    return this.http.get<Establishment[]>(this.apiUrl).pipe(
-      map((response: Establishment[]) => {
-        return response;
-      })
-    );
+    return this.http
+      .get<Establishment[]>(`${environment.apiUrl}/establishments`)
+      .pipe(
+        map((response: Establishment[]) => {
+          return response;
+        })
+      );
   }
 
   postList(establishments: Establishment[]): Observable<any> {
     const headers = new HttpHeaders({ "Content-Type": "application/json" });
-    return this.http.post(this.apiUrl, establishments, { headers: headers });
+    return this.http.post(
+      `${environment.apiUrl}/establishments`,
+      establishments,
+      { headers: headers }
+    );
   }
 }
