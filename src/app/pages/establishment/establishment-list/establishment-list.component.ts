9300aceb64dd1cea9b91cbeafa29af0c66211fc2
Aaron Sisler <aaronsisler@users.noreply.github.com>
2025-05-22 21:40:17 -0400

    Feature/material (#35)

--- src/app/pages/establishment/establishment-list/establishment-list.component.ts
+++ src/app/pages/establishment/establishment-list/establishment-list.component.ts
@@ -1,16 +1,19 @@
 import { Component, inject, OnInit } from "@angular/core";
+import { MatTableModule } from "@angular/material/table";
+
 import { Establishment } from "../establishment";
-import { EstablishmentService } from "../../../core/services/establishment-service";
 import { EstablishmentStore } from "../../../core/stores/establishment.store";
 
 @Component({
   selector: "app-establishment-list",
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: "./establishment-list.component.html",
   styleUrl: "./establishment-list.component.scss",
 })
 export class EstablishmentListComponent implements OnInit {
   establishmentList: Establishment[] = [];
+  displayedColumns: string[] = ["establishment-id", "name"];
+
   readonly establishmentStore = inject(EstablishmentStore);
 
   ngOnInit(): void {
