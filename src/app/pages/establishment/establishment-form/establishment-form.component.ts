92f8eee12bd617500415c873caba11101e427e6d
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-05-22 21:29:50 -0400

    Adding in the logic for Material UI and form/table

--- src/app/pages/establishment/establishment-form/establishment-form.component.ts
+++ src/app/pages/establishment/establishment-form/establishment-form.component.ts
@@ -1,22 +1,36 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, ViewChild } from "@angular/core";
 import {
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
+  FormsModule,
   Validators,
+  FormGroupDirective,
+  NgForm,
 } from "@angular/forms";
-import { EstablishmentService } from "../../../core/services/establishment-service";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatButtonModule } from "@angular/material/button";
+
 import { EstablishmentStore } from "../../../core/stores/establishment.store";
 
 @Component({
   selector: "app-establishment-form",
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+
   templateUrl: "./establishment-form.component.html",
   styleUrl: "./establishment-form.component.scss",
 })
 export class EstablishmentFormComponent {
   formGroup: FormGroup;
-  establishmentService = inject(EstablishmentService);
+  @ViewChild(FormGroupDirective) formDirective!: FormGroupDirective;
+
   readonly establishmentStore = inject(EstablishmentStore);
 
   constructor(private fb: FormBuilder) {
@@ -28,5 +42,6 @@ export class EstablishmentFormComponent {
   handleSubmit() {
     this.establishmentStore.postAll([this.formGroup.value]);
     this.formGroup.reset();
+    this.formDirective.resetForm();
   }
 }
