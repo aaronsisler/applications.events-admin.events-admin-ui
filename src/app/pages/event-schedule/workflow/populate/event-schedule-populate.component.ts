eb03ce0d471a31f31f45a0510dd0ad2395233ef1
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-05-26 07:55:27 -0400

    Have the dyamic control adding working. Need to get the event info to populate on creation now

--- src/app/pages/event-schedule/workflow/populate/event-schedule-populate.component.ts
+++ src/app/pages/event-schedule/workflow/populate/event-schedule-populate.component.ts
@@ -2,12 +2,59 @@ import { Component } from "@angular/core";
 import { EventSelectorComponent } from "./event-selector/event-selector.component";
 import { EventStore } from "../../../../core/stores/event.store";
 import { ScheduledEventListComponent } from "./scheduled-event-list/scheduled-event-list.component";
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-event-schedule-populate",
-  imports: [EventSelectorComponent, ScheduledEventListComponent],
+  imports: [
+    EventSelectorComponent,
+    ScheduledEventListComponent,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    CommonModule,
+  ],
   templateUrl: "./event-schedule-populate.component.html",
   styleUrl: "./event-schedule-populate.component.scss",
   providers: [EventStore],
 })
-export class EventSchedulePopulateComponent {}
+export class EventSchedulePopulateComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      items: this.fb.array([]),
+    });
+
+    console.log(this.items);
+  }
+
+  get items(): FormArray {
+    return this.form.get("items") as FormArray;
+  }
+
+  addItem() {
+    const group = this.fb.group({
+      name: ["", Validators.required],
+      description: [""],
+    });
+    this.items.push(group); // now items is FormArray of FormGroup
+  }
+
+  removeItem(index: number) {
+    this.items.removeAt(index);
+  }
+
+  onSubmit() {
+    console.log("Form values:", this.form.value);
+  }
+}
