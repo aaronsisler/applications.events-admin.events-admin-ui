d984d1f4c90bd9328f4daf18b524544211b0a489
Aaron Sisler <aaronsisler@users.noreply.github.com>
2025-06-01 08:21:09 -0400

    Feature/csv link (#41)

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   apiUrl: "http://localhost:8080",
+  storageUrl: "http://localhost:4566",
+  storageFolder: "event-admin-service-file-storage",
   userId: "aaron-sisler-id",
 };
